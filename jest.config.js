c93e51b908900d1cabe304d0aa09b200db7f4dd8
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2021-11-02 15:06:40 +0100

    build: remove all packages and configs already present in 'client' folder

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
-module.exports = {
-  // The test environment that will be used for testing
-  testEnvironment: "jsdom"
-};
