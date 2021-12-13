65a823e45bfeac6578bce6ecdc0bb218bb570493
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2021-12-13 14:45:21 +0100

    chore: hard-code data we want to display on home page

---
+++ client/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+  posts: [
+    {
+      id: 1,
+      author: "Andrew Vaughn",
+      date: "JUNE 08, 2021",
+      title: "The Plan for React 18",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel orci porta non pulvinar. Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 2,
+      author: "Brian Clark",
+      date: "DECEMBER 21, 2020",
+      title: "Introducing the New JSX Transform",
+      content:
+        "Leo vel orci porta non pulvinar. Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 3,
+      author: "Christine Nabors",
+      date: "OCTOBER 20, 2020",
+      title: "React Conf recap: Hooks, Suspense, and Concurrent Rendering",
+      content:
+        "Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 4,
+      author: "Rachel Abernathy",
+      date: "SEPTEMBER 22, 2020",
+      title: "React v17.0",
+      content:
+        "Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 5,
+      author: "Andrew Vaughn",
+      date: "AUGUST 10, 2020",
+      title: "The Plan for React 18",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel orci porta non pulvinar. Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 6,
+      author: "Brian Clark",
+      date: "FEBRUARY 26, 2020",
+      title: "Introducing the New JSX Transform",
+      content:
+        "Leo vel orci porta non pulvinar. Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 7,
+      author: "Christine Nabors",
+      date: "NOVEMBER 06, 2019",
+      title: "React Conf recap: Hooks, Suspense, and Concurrent Rendering",
+      content:
+        "Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 8,
+      author: "Rachel Abernathy",
+      date: "OCTOBER 22, 2019",
+      title: "React v17.0",
+      content:
+        "Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+    {
+      id: 9,
+      author: "Andrew Vaughn",
+      date: "AUGUST 15, 2019",
+      title: "The Plan for React 18",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel orci porta non pulvinar. Natoque penatibus et magnis dis parturient. Etiam erat velit scelerisque in dictum non consectetur a erat. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Enim tortor at auctor urna nunc id cursus. Ultricies integer quis auctor elit sed vulputate. Nunc sed velit dignissim sodales ut eu sem integer. Faucibus nisl tincidunt eget nullam non nisi est sit. Et netus et malesuada fames ac.",
+    },
+  ],
+};
+
+export default data;
