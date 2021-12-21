aab397761b738c5af111a8dc0109e867c9730d5e
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2021-12-21 18:09:16 +0100

    style: styling the post list

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -1,13 +1,22 @@
 import React from "react";
 import PostListItem from "../PostListItem/PostListItem";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  gridItem: {
+    borderTop: "1px dotted #D0D0D0",
+  },
+});
 
 export default function PostList({ posts }) {
+  const classes = useStyles();
+
   return (
-    <Grid container>
+    <Grid container spacing={2}>
       {posts.map((post) => {
         return (
-          <Grid item key={post.id} xs={4}>
+          <Grid item key={post.id} xs={4} className={classes.gridItem}>
             <PostListItem post={post} />
           </Grid>
         );
--- client/src/components/PostListItem/PostListItem.js
+++ client/src/components/PostListItem/PostListItem.js
@@ -3,19 +3,59 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    margin: theme.spacing(2, 0),
+  },
+  title: {
+    fontWeight: 600,
+    color: "#282C34",
+  },
+  div: {
+    fontWeight: 400,
+    color: "#6D6D6D",
+  },
+  author: {
+    fontWeight: 500,
+    color: "#282C34",
+  },
+  content: {
+    textOverflow: "ellipsis",
+  },
+}));
 
 export default function PostListItem({ post }) {
+  const classes = useStyles();
+
   return (
-    <Card>
+    <Card square elevation={0} className={classes.card}>
       <CardActionArea>
         <CardContent>
-          <Typography variant="h5" component="h2">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            className={classes.title}
+          >
             {post.title}
           </Typography>
-          <Typography variant="subtitle2" component="div">
-            by <span>{post.author}</span> - <span>{post.date}</span>
+          <Typography
+            gutterBottom
+            variant="subtitle2"
+            component="div"
+            className={classes.div}
+          >
+            by <span className={classes.author}>{post.author}</span> -{" "}
+            <span>{post.date}</span>
           </Typography>
-          <Typography variant="body2" component="p">
+          <Typography
+            noWrap
+            variant="body2"
+            component="p"
+            className={classes.content}
+          >
             {post.content}
           </Typography>
         </CardContent>
