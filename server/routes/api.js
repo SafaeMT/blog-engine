42107db762add62fd619ecf6df48210a8f8caa4b
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2021-12-31 17:51:32 +0100

    feat: add API routes for needed GET requests

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,4 +6,17 @@ router.get("/", function (req, res) {
   res.send({ message: "Express backend is here!" });
 });
 
+// Request URL must include the 'limit' query parameter (key & value)
+// 'limit' value indicates the number of posts we want to get from database
+router.get("/posts", function (req, res) {
+  if (!req.query.limit) {
+    res.status(400).end();
+  }
+  res.send({ message: `GET ${req.query.limit} posts` });
+});
+
+router.get("/post/:id", function (req, res) {
+  res.send({ message: `GET the post that has ID = ${req.params.id}` });
+});
+
 module.exports = router;
