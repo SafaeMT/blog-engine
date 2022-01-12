4747da2ce34827f351da9ff82f0f4956cc3959cb
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2022-01-12 18:37:08 +0100

    chore: insert fake docs into posts collection

--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+const posts = require("../data/posts");
 
 const DB_PORT = process.env.DB_PORT;
 const PROTOCOL = process.env.DB_PROTOCOL;
@@ -7,6 +8,7 @@ const USER = process.env.MONGO_INITDB_ROOT_USERNAME;
 const PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
 const HOST = process.env.DB_HOST;
 const URI = `${PROTOCOL}://${USER}:${PASSWORD}@${HOST}:${DB_PORT}`;
+const DB_NAME = process.env.DB_NAME;
 
 async function makeDb() {
   // Create the database client
@@ -16,19 +18,19 @@ async function makeDb() {
   });
 
   // Connect to database
-  client
-    .connect()
-    .then(async () => {
-      console.log(">> Connected correctly to the database");
-      const db = client.db();
-      const doc = { title: "Alpha Centauri", category: "Astronomy" };
-      const result = await db.collection("articles").insertOne(doc);
-      console.log(`The document was inserted with the _id: ${result.insertedId}`);
-    })
-
-    .catch((err) => {
-      console.log(err.stack);
-    });
+  try {
+    await client.connect();
+    console.log(">> Connected correctly to the database");
+    const db = client.db(DB_NAME);
+    if ((await db.collection("posts").countDocuments()) !== 0) {
+      await db.collection("posts").drop();
+    }
+    const result = await db.collection("posts").insertMany(posts);
+    console.log(`>> ${result.insertedCount} documents have been inserted`);
+    return db;
+  } catch (err) {
+    console.log(err.stack);
+  }
 }
 
 module.exports = makeDb;
