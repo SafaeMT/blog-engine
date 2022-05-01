cba475623b94d9ea83d95e2754bd846afa2827a6
SafaeMT <57368349+SafaeMT@users.noreply.github.com>
2022-05-01 13:30:39 +0200

    feat(frontend): include an 'add post' icon button in homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import PostList from "../../components/PostList/PostList";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import PostAddIcon from "@material-ui/icons/PostAdd";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -19,11 +21,39 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   title: {
-    margin: theme.spacing(5, "auto"),
+    margin: theme.spacing(5, 0),
     fontWeight: 600,
     color: "#282C34",
     [theme.breakpoints.down("sm")]: {
-      margin: theme.spacing(3, "auto"),
+      margin: theme.spacing(3, 0),
+    },
+  },
+  iconButton: {
+    zIndex: 1000,
+    position: "fixed",
+    top: theme.spacing(19),
+    right: theme.spacing(11),
+    padding: theme.spacing(1),
+    color: "white",
+    backgroundColor: "#61dafb",
+    "&.MuiButtonBase-root:hover": {
+      backgroundColor: "#61dafb",
+    },
+    border: "1px solid #61dafb",
+    boxShadow: "3px 3px 5px #6D6D6D",
+    [theme.breakpoints.down("md")]: {
+      top: theme.spacing(17),
+      right: theme.spacing(10),
+    },
+    [theme.breakpoints.down("sm")]: {
+      top: "auto",
+      bottom: theme.spacing(6),
+      right: theme.spacing(6),
+    },
+    [theme.breakpoints.down("xs")]: {
+      top: "auto",
+      bottom: theme.spacing(4),
+      right: theme.spacing(4),
     },
   },
 }));
@@ -37,6 +67,9 @@ export default function Home() {
         <Typography variant="h2" component="h1" className={classes.title}>
           Recent Posts
         </Typography>
+        <IconButton aria-label="add a new post" className={classes.iconButton}>
+          <PostAddIcon fontSize="large" />
+        </IconButton>
         <PostList />
       </Container>
     </div>
